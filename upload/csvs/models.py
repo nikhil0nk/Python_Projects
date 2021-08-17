784c4ef03c16d5d8ec28cc7f4427bd97ac8329ab
nikhil0nk <nikhilkharbanda98@gmail.com>
2021-08-17 10:55:58 +0530

    No Change

--- upload/csvs/models.py
+++ upload/csvs/models.py
@@ -7,10 +7,10 @@ TF = (
 )
 
 class Items(models.Model):
-    category    = models.CharField(max_length=30)
-    prompts     = models.CharField(max_length=400, blank=True)
-    keywords    = models.CharField(max_length=100, blank=True)
-    cons        = models.CharField(max_length=100, blank=True)
-    status      = models.CharField(max_length=15, choices=TF, default="Negative")
+  category    = models.CharField(max_length=30)
+  prompts     = models.CharField(max_length=400, blank=True)
+  keywords    = models.CharField(max_length=100, blank=True)
+  cons        = models.CharField(max_length=100, blank=True)
+  status      = models.CharField(max_length=15, choices=TF, default="Negative")
 		
 
