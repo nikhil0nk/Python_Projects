f515c645719bba0ea99e99e16a9acc8f88987084
nikhil0nk <nikhilkharbanda98@gmail.com>
2021-10-01 00:06:11 +0530

    No Changes

--- gtrend_analysis.py
+++ gtrend_analysis.py
@@ -17,7 +17,7 @@ def get_trend(keyword, date_time):
   data_interest.drop(data_interest.columns[len(data_interest.columns)-1], axis=1, inplace=True)
 
   # Excel dump
-  """
+   
   try:
     book = load_workbook("googletrend_data.xlsx")
     writer = pd.ExcelWriter("googletrend_data.xlsx", engine = 'openpyxl')
@@ -29,6 +29,7 @@ def get_trend(keyword, date_time):
     data_interest.to_excel(writer, sheet_name=keyword[0])
     writer.save()
   
+  
   # Graphical representation
   plt.xlabel("Date")
   plt.ylabel(keyword[0])
@@ -36,7 +37,7 @@ def get_trend(keyword, date_time):
   plt.plot(data_interest, color='blue', label='original')
   plt.plot(data_interest.rolling(window=4).mean(), color='red', label='mean')
   plt.xticks(rotation=90)
-  plt.savefig("trend"+keyword[0]+".png")
+  plt.savefig(keyword[0]+"_trend.png")
   plt.close()
 
   # Log Scale Graph
@@ -48,9 +49,9 @@ def get_trend(keyword, date_time):
   plt.plot(data_interest_log, color='black', label='log')
   plt.plot(data_interest_log.rolling(window=4).mean(), color='green', label='log mean')
   plt.xticks(rotation=90)
-  plt.savefig("log_trend"+keyword[0]+".png")
+  plt.savefig(keyword[0]+"_log_trend.png")
   plt.close()
-  """
+  
 
   dt_mean = re.findall("\d+\.\d+", str(data_interest.mean()))
   dt_mean = float(dt_mean[0])
@@ -62,13 +63,15 @@ def get_trend(keyword, date_time):
   else:
     print(keyword[0], "is in \u001b[31mdowntrend\u001b[37m")
   
-  print(data_interest.head(3))
+  return data_interest
 
 kw_list = [
-  "Countertop Microwave Ovens", "Stand Mixers", "TVs", "Portable Air Conditioners", "Inkjet Printers", "Air Fryers", "Projectors", "Cordless Drills",
-  "Stand-Up Paddleboards", "Treadmills", "Smartwatches"
+  # "Countertop Microwave Ovens", "Stand Mixers", "TVs", "Portable Air Conditioners", "Inkjet Printers",
+  # "Air Fryers", "Projectors", "Cordless Drills", "Stand-Up Paddleboards", "Treadmills",
+  "Smartwatches"
   ] #List of Categories
 for kw in kw_list:
   keyword.append(kw)
   get_trend(keyword, '2018-09-22 2021-09-21')
   keyword.pop()
+
